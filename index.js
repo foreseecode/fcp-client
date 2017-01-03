841cc2e9c840c74cce8f736ae7331bbce172a0ce
Alexei White <alexei.white@answers.com>
2017-01-02 17:56:35 -0800

    Posting gateway files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var jsZip = require('node-zip'),
+  fs = require('fs'),
+  rest = require('restler');
+
 /**
  * Initialize a new instance of FCP Client
  * @param username
@@ -15,18 +19,31 @@ var FCPClient = function (username, password, hostname) {
   if (!hostname) {
     throw new Error("Missing hostname");
   }
+  if (hostname.indexOf(":/") == -1) {
+    throw new Error("Hostname should look like https://bla.bla.com with no trailing slashes");
+  }
   this.username = username;
   this.password = password;
   this.hostname = hostname;
 };
 
 /**
+ * Return a fully qualified URL for an endpoint
+ * @param endpoint
+ * @private
+ */
+FCPClient.prototype._constructEndpointURL = function(endpoint) {
+  return this.hostname + "/" + endpoint;
+};
+
+/**
  * Post new gateway JS files
  * @param uniminifiedJSStr {String} String containing the unminified JS file
  * @param minifiedJSStr {String} String containing the minified JS file
+ * @param notes {String} Comments on this release
  * @param callback {Function} Callback
  */
-FCPClient.prototype.postGatewayFiles = function (uniminifiedJSStr, minifiedJSStr, callback) {
+FCPClient.prototype.postGatewayFiles = function (uniminifiedJSStr, minifiedJSStr, notes, callback) {
   callback = callback || function () {
     };
   if (!uniminifiedJSStr) {
@@ -35,9 +52,28 @@ FCPClient.prototype.postGatewayFiles = function (uniminifiedJSStr, minifiedJSStr
   if (!minifiedJSStr) {
     throw new Error("Missing minified JS file.");
   }
+  if (!notes) {
+    throw new Error("Missing notes.");
+  }
   if (minifiedJSStr.length >= uniminifiedJSStr) {
     throw new Error("The minified JS file appears to be the same size or larger than the uniminified version.");
   }
+  var zip = new jsZip();
+  zip.file('gateway.js', uniminifiedJSStr);
+  zip.file('gateway.min.js', minifiedJSStr);
+  var data = zip.generate({base64: false, compression: 'DEFLATE'});
+
+  rest.post(this._constructEndpointURL('gateway'), {
+    multipart: true,
+    username: this.username,
+    password: this.password,
+    data: {
+      'notes': notes,
+      'file': rest.data("gateway.zip", "application/octet-stream", data)
+    }
+  }).on('complete', function(data) {
+    console.log(data);
+  });
 };
 
 // Tell the world
