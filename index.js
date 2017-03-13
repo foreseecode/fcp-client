a2a295219151477d1728cf3e2080d39589b214f7
Pablo Suarez <pablo97suarez@gmail.com>
2017-03-13 14:16:10 -0700

    adding back the disable env prompt option for push_code_all_env

--- index.js
+++ index.js
@@ -746,6 +746,10 @@ FCPClient.prototype.pushCustomerConfigForProduct = function (clientid, sitekey, 
   }.bind(this));
 };
 
+FCPClient.environmentShort = [
+  "dev", "qa", "qa2", "stg", "prod", "local"
+];
+
 /**
  * Defines environments
  * @type {{}}
@@ -772,7 +776,6 @@ FCPClient.frontEndEnvironments = {
   "prod": "https://gateway.foresee.com"
 };
 
-
 /**
  * Ask the user for credentials and notes if appropriate
  * @param donotes {Boolean} Ask for notes?
@@ -798,15 +801,16 @@ FCPClient.promptForFCPCredentials = function (options, cb) {
     password = ev.FCP_PASSWORD;
     notes = ev.FCP_NOTES;
     environment = ev.FCP_ENVIRONMENT;
-    //latest = ev.FCP_LATEST || latest;
   } catch (e) {
   }
+  // Read FCP credentials from environment variables, if they exist
   if (!username) {
     try {
       username = process.env.FCP_USERNAME;
       password = process.env.FCP_PASSWORD;
+      notes = process.env.FCP_NOTES;
+      environment = process.env.FCP_ENVIRONMENT;
     } catch (e) {
-
     }
   }
 
@@ -842,7 +846,7 @@ FCPClient.promptForFCPCredentials = function (options, cb) {
     };
     console.log("Latest: true/false.".yellow);
   }
-  if (typeof(environment) == "undefined") {
+  if (!options.disableEnv && typeof(environment) == "undefined") {
     schema.properties.environment = {
       required: true,
       type: 'integer',
@@ -868,25 +872,13 @@ FCPClient.promptForFCPCredentials = function (options, cb) {
         result.latest = latest;
       }
       result.env = result.environment;
-      if (result.environment == 0) {
-        result.environment = FCPClient.environments.dev;
-        result.frontEndEnvironment = FCPClient.frontEndEnvironments.dev;
-      } else if (result.environment == 1) {
-        result.environment = FCPClient.environments.qa;
-        result.frontEndEnvironment = FCPClient.frontEndEnvironments.qa;
-      } else if (result.environment == 2) {
-        result.environment = FCPClient.environments.qa2;
-        result.frontEndEnvironment = FCPClient.frontEndEnvironments.qa2;
-      } else if (result.environment == 3) {
-        result.environment = FCPClient.environments.stg;
-        result.frontEndEnvironment = FCPClient.frontEndEnvironments.stg;
-      } else if (result.environment == 4) {
-        result.environment = FCPClient.environments.prod;
-        result.frontEndEnvironment = FCPClient.frontEndEnvironments.prod;
-      } else if (result.environment == 5) {
-        result.environment = FCPClient.environments.local;
-        result.frontEndEnvironment = FCPClient.frontEndEnvironments.local;
-      } else {
+
+      if (result.env >= 0 && result.env <= 5) {
+        // dev, qa, qa2, stg, prod, local
+        var es = FCPClient.environmentShort[result.env];
+        result.environment = FCPClient.environments[es];
+        result.frontEndEnvironment = FCPClient.frontEndEnvironments[es];
+      } else if (!options.disableEnv) {
         throw new Error("Invalid environment.");
       }
       cb(result);
